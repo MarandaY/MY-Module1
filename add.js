2cf74c6b1ce872887a686d25257ca650c646bfb6
MarandaY <mybarbo5@student.alamo.edu>
2023-01-19 18:57:42 -0600

    image fix

--- add.js
+++ add.js
@@ -1,7 +1,7 @@
 console.log ("What are your favorite types of music, if you have any?");
 
 function changeImage() {
-    document.getElementById("drawing").src = "/images/grusha.png";
+    document.getElementById("drawing").src = "images/grusha.png";
   }
 
 document.write("<p>I decided to use some of my art to test out some JavaScript.</p> <p>The question should be located in the console!</p>" , Date());
